761db1ba564664106397c77ae4d93843a6a336b0
johnafekuana <afekuanajohn@gmail.com>
2024-03-07 01:40:02 +0100

    created context for Wallet Provider with the WalletProvider.jsx file

---
+++ context/WalletConnectionProvider.jsx
@@ -0,0 +1,31 @@
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { 
+	PhantomWalletAdapter,
+	SolflareWalletAdapter,
+	TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+
+import { useMemo } from "react";
+
+
+const WalletConnectionProvider = ({children}) => {
+	const endpoint = useMemo(()=> "https://api.devnet.solana.com",[]);
+
+	const wallets = useMemo(() => [
+		new PhantomWalletAdapter(),
+		new SolflareWalletAdapter(),
+		new TorusWalletAdapter(),
+	], []);
+
+
+	return(
+		<ConnectionProvider endpoint={endpoint}>
+			<WalletProvider wallets={wallets} autoConnect>
+				<WalletModalProvider>{children}</WalletModalProvider>
+			</WalletProvider>
+		</ConnectionProvider>
+	)
+}
+
+export default WalletConnectionProvider; 
